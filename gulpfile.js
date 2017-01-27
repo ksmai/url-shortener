51ad8726d5676299d5370f1c6cfb5cd33bc0a860
ksmai <kasingmai@gmail.com>
2017-01-27 14:57:14 +0800

    Set up dependencies and bui build process for client-side scripts

--- gulpfile.js
+++ gulpfile.js
@@ -1,13 +1,47 @@
 'use strict';
 const gulp = require('gulp');
 const mocha = require('gulp-mocha');
+const minifyHTML = require('gulp-minify-html');
+const cleanCSS = require('gulp-clean-css');
+const uglify = require('gulp-uglify');
+const browserify = require('gulp-browserify');
+const babel = require('gulp-babel');
+const concat = require('gulp-concat');
 
 gulp.task('test-server', function() {
   gulp.src(['./src/server/**/*.test.js'])
       .pipe(mocha());
 });
 
-gulp.task('watch', function() {
+gulp.task('minhtml', function() {
+  gulp.src(['./src/client/**/*.html'])
+      .pipe(minifyHTML())
+      .pipe(gulp.dest('./bin'));
+});
+
+gulp.task('mincss', function() {
+  gulp.src(['./src/client/**/*.css'])
+      .pipe(cleanCSS())
+      .pipe(concat('styles.css'))
+      .pipe(gulp.dest('./bin'));
+});
+
+gulp.task('browserify', function() {
+  gulp.src('./src/client/app.js')
+      .pipe(browserify())
+      .pipe(babel({
+        presets: 'es2015'
+      }))
+      .pipe(uglify())
+      .pipe(gulp.dest('./bin'));
+});
+
+gulp.task('build', ['minhtml', 'mincss', 'browserify']);
+
+gulp.task('watch', ['build'], function() {
   gulp.watch(['./src/server/**/*.js'], ['test-server']);
+  gulp.watch(['./src/client/**/*.html'], ['minhtml']);
+  gulp.watch(['./src/client/**/*.css'], ['mincss']);
+  gulp.watch(['./src/client/**/*.js'], ['browserify']);
 });
 
