53420040bad53018c1ebd15d20a0d480d39f6e24
ksmai <kasingmai@gmail.com>
2017-01-27 17:21:35 +0800

    Factor out regular expression for URL

--- src/server/schemas/url-schema.js
+++ src/server/schemas/url-schema.js
@@ -1,12 +1,10 @@
 'use strict';
 const mongoose = require('mongoose');
 
-const urlRegExp = /(([\w]+:)?\/\/)?(([\d\w]|%[a-fA-f\d]{2,2})+(:([\d\w]|%[a-fA-f\d]{2,2})+)?@)?([\d\w][-\d\w]{0,253}[\d\w]\.)+[\w]{2,63}(:[\d]+)?(\/([-+_~.\d\w]|%[a-fA-f\d]{2,2})*)*(\?(&?([-+_~.\d\w]|%[a-fA-f\d]{2,2})=?)*)?(#([-+_~.\d\w]|%[a-fA-f\d]{2,2})*)?/;
-
 const urlSchema = {
   url: {
     type: String,
-    match: urlRegExp,
+    match: require('../../util/url-reg-exp'),
     required: true,
     unique: true
   }
